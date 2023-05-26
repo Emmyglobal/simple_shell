447fd42176bbaad1b5d2e9591d45d5c4310e1b31
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-05-26 04:25:23 -0700

    first attempt

--- betty.c
+++ betty.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+/*
+ * main -> A function of a simple shell
+ * @ac: argument count
+ * @av: argument vector
+ */
+
+int main(int ac, char *av[])
 {
-	int pass;
+	char *command = NULL;
+	size_t len = 0;
 
-	pass = 20 * 5;
+	printf("%i\n", ac);
+	printf("%s", av[0]);
 
-	printf("The pass mark must be %i\n", pass);
+	while (1)
+	{
+		printf("EMMYTECH ");
+		getline(&command, &len, stdin);
+		printf("%s", command);
+	}
+
+	free(command);
+	return (0);
 }
